f73bcbb2b66ac8fa5e8683a67aa6996229caa981
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2023-02-08 16:30:05 +0900

    mix nerves.new

---
+++ lib/ping_pong_measurer_rclex_on_nerves.ex
@@ -0,0 +1,18 @@
+defmodule PingPongMeasurerRclexOnNerves do
+  @moduledoc """
+  Documentation for PingPongMeasurerRclexOnNerves.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PingPongMeasurerRclexOnNerves.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ping_pong_measurer_rclex_on_nerves_test.exs
@@ -0,0 +1,8 @@
+defmodule PingPongMeasurerRclexOnNervesTest do
+  use ExUnit.Case
+  doctest PingPongMeasurerRclexOnNerves
+
+  test "greets the world" do
+    assert PingPongMeasurerRclexOnNerves.hello() == :world
+  end
+end

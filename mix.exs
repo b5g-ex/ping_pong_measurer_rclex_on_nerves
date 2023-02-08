857ff541a56695ed58d0fe4823518329cc000aed
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2023-02-08 16:42:31 +0900

    add rclex to deps

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule PingPongMeasurerRclexOnNerves.MixProject do
       {:shoehorn, "~> 0.9.1"},
       {:ring_logger, "~> 0.8.5"},
       {:toolshed, "~> 0.2.26"},
+      {:rclex, "~> 0.8.3"},
 
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.13.0", targets: @all_targets},
